8341a92a90b115fcefbbc52b0f1946839d0e17a2
jardeylson jacinto <jardeylsong.m@gmail.com>
2020-06-17 12:54:00 -0300

    projeto criado

---
+++ src/aplication/funcionario.java
@@ -0,0 +1,34 @@
+package aplication;
+
+public class funcionario {
+    private string name;
+    private integer number;
+    private string cpf;
+
+    public funcionario() {
+    }
+
+    public string getName() {
+        return name;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+
+    public integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(integer number) {
+        this.number = number;
+    }
+
+    public string getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(string cpf) {
+        this.cpf = cpf;
+    }
+}
---
+++ src/aplication/main.java
@@ -0,0 +1,8 @@
+package aplication;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("olá mundo");
+        new funcionario();
+    }
+}
